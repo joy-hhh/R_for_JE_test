b3c5959e919899785445b73af62119d218f5a903
joy-hhh <joyhhh@outlook.kr>
2021-10-01 01:24:20 +0900

    mus app

--- mus.R
+++ mus.R
@@ -1,23 +1,25 @@
 library(tidyverse)
 
+
 je <- read_csv("~/R/R_for_JE_test/je_utf.csv")
 sales <- filter(je, ACCTCD == 40401)
 
 
 pm <- 300000000
-amount = "CR"
+# rename(je, CR = 금액열이름)
 
 
 first_pm <- pm
 sampling <- vector()
 for (i in seq_along(sales$CR)) {
-  if (pm - sales[i, amount] >0) { 
-    pm = pm - sales[i, amount]
+  if (pm - sales[i, "CR"] >0) { 
+    pm = pm - sales[i, "CR"]
   } else { 
-    pm = pm - sales[i, amount] + first_pm
+    pm = pm - sales[i, "CR"] + first_pm
     sampling <- c(sampling, i)
   }
 }
 
-test_sample <- sales[sampling,]
 
+test_sample <- sales[sampling,]
+write_excel_csv(test_sample, "test_sample.csv")
