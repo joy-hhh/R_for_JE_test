2eae121b78310b795183a2c3bdb0960ce0936d59
joy-hhh <joyhhh@outlook.kr>
2021-10-01 00:33:43 +0900

    mus2

--- mus.R
+++ mus.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-setwd("~/R")
 
 je <- read_csv("~/R/R_for_JE_test/je_utf.csv")
 sales <- filter(je, ACCTCD == 40401)
@@ -13,10 +12,8 @@ first_pm <- pm
 sampling <- vector()
 for (i in seq_along(sales$CR)) {
   if (pm - sales[i, amount] >0) { 
-    sales[i, "temp"] = pm - sales[i, amount]
     pm = pm - sales[i, amount]
   } else { 
-    sales[i, "temp"] = pm - sales[i, amount] + first_pm
     pm = pm - sales[i, amount] + first_pm
     sampling <- c(sampling, i)
   }
