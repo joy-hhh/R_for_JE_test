7920f63f9fba0759662266d5672fec4c29b46858
joy-hhh <joyhhh@outlook.kr>
2024-02-11 19:47:07 +0900

    relative modi

--- sh/relative.sh
+++ sh/relative.sh
@@ -3,26 +3,26 @@
 # 입력 파일의 이름
 input_file="$1"
 
-# 출력 파일의 이름
-output_file="$2"
-
 # 필터링할 열의 번호
-column1="$3"
+column1="$2"
 
 # 필터링할 값의 정규식
-regex1="$4"
+regex1="$3"
 
 # Key 값인 전표번호 열의 번호
-column2="$5"
+column2="$4"
+
+# 출력 파일의 이름
+output_file="$5"
 
 
 temp_file="temp.txt"
 
 cat "$input_file" | grep "$regex1" > "temp_1.txt"
-cut -f"$5" "temp_1.txt" > "temp_2.txt"
+cut -f"$column2" "temp_1.txt" > "temp_2.txt"
 sort -u "temp_2.txt" > "temp_3.txt"
 
-cut -f"$5","$3" "$input_file" > "$temp_file"
+cut -f"$column2",$column1" "$input_file" > "$temp_file"
 
 sort -t$'\t' -k1,1 "temp_3.txt" -o "temp_1.txt"
 sort -t$'\t' -k1,1 "$temp_file" -o "temp_2.txt"
--- sh/relativeJE.sh
+++ sh/relativeJE.sh
@@ -3,26 +3,27 @@
 # 입력 파일의 이름
 input_file="$1"
 
-# 출력 파일의 이름
-output_file="$2"
-
 # 계정과목열의 번호
-column1="$3"
+column1="$2"
 
 # 필터링할 값
-regex1="$4"
-regex2="$5"
+regex1="$3"
+regex2="$4"
 
 # 전표번호 열(Key)의 번호
-column2="$6"
+column2="$5"
+
+# 출력 파일의 이름
+output_file="$6"
+
 
 temp_file="temp.txt"
 cat "$input_file" | grep "$regex1" > "temp_1.txt"
 cat "$input_file" | grep "$regex2" > "temp_2.txt"
-cut -f"$6" "temp_1.txt" | sort -u > "temp_3.txt"
-cut -f"$6" "temp_2.txt" | sort -u > "temp_4.txt"
+cut -f"$column2" "temp_1.txt" | sort -u > "temp_3.txt"
+cut -f"$column2" "temp_2.txt" | sort -u > "temp_4.txt"
 
-cut -f"$6"- "$input_file" > "$temp_file"
+cut -f"$column2"- "$input_file" > "$temp_file"
 
 sort -t$'\t' -k1,1 "temp_3.txt" -o "temp_1.txt"
 sort -t$'\t' -k1,1 "temp_4.txt" -o "temp_2.txt"
