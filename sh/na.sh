79650ab30ab5582bee7346249bdfd8e325ee656b
joy-hhh <joyhhh@outlook.kr>
2024-02-13 22:12:59 +0900

    na.sh

---
+++ sh/na.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 입력 파일의 이름
+input_file="$1"
+
+# 열 개수 세기
+colnum=`awk -F'\t' '{print NF; exit}' "$1"`
+
+awk -F"\t" '{ for (i=1; i<='"$colnum"'; i++) { if ($i == "NA") { count[i]++ }}} END { for (i=1; i<='"$colnum"'; i++) { printf "%d\t", count[i] } printf "\n"}' "$input_file" 
